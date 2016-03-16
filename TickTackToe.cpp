26dbb70c9c98cba813783b28d26ed2a58f3543f4
214502730 <shanerdewar@gmail.com>
2016-03-16 12:01:45 +0200

    added move method

--- TickTackToe.cpp
+++ TickTackToe.cpp
@@ -13,12 +13,32 @@ TicTacToe::TicTacToe()
 void TicTacToe::reset()
 {
 	int a, b;
-	for (a = 0; a < 3; a++){
-		for (b = 0; b < 3; b++){
+	for (a = 1; a < 4; a++)
+	{
+		for (b = 1; b < 4; b++)
+		{
 			Board[i][j] = NULL;
 		}
 	}
 }
+
+//determines if the players move is correct or not
+bool TicTacToe::move(int row, int col)
+{
+	if (Board[row][col] == NULL)
+	{
+		if (row < 4 && row > 0 && col < 4 && col > 0)
+		{
+			return true;
+		}
+	}
+	else
+	{
+	  return false;
+	}
+}
+
+
 
 //default destructor
 TicTacToe::~TicTacToe()
@@ -26,5 +46,7 @@ TicTacToe::~TicTacToe()
 }
 
 
-int main(){
-	}
+int main()
+{
+
+}
