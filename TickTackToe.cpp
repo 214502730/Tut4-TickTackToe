428634a24a55df0e8189b7123543b04c22179385
214502730 <shanerdewar@gmail.com>
2016-03-16 11:53:24 +0200

    initial commit-constuctor,destructor,reset method

---
+++ TickTackToe.cpp
@@ -0,0 +1,30 @@
+#include "TicTacToe.h"
+#include <iostream>
+
+using namespace std;
+
+//default constructor
+TicTacToe::TicTacToe()
+{
+	
+}
+
+//clears the board and sets all values to 0
+void TicTacToe::reset()
+{
+	int a, b;
+	for (a = 0; a < 3; a++){
+		for (b = 0; b < 3; b++){
+			Board[i][j] = NULL;
+		}
+	}
+}
+
+//default destructor
+TicTacToe::~TicTacToe()
+{
+}
+
+
+int main(){
+	}
